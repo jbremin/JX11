4be006acf421eee30ae44006cd7bac312c2f49de
Johan Bremin <johanbremin@Johans-MBP-4.home>
2023-12-28 22:34:22 +0100

    Routed filtergraph to output. Also changed so only stereo output is supported by JX11.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -114,8 +114,7 @@ bool JX11AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) con
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
