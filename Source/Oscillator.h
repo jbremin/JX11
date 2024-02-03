6b3f5c6d65d42830f9ae6a102b3714c3d34e0b25
Johan Bremin <jbremin@hotmail.com>
2024-02-03 23:40:24 +0100

    Made the sawtooth bandlimited to avoid aliasing.

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -12,21 +12,45 @@
 
 const float TWO_PI = 6.2831853071795864f;
 
-class Oscillator {
+class Oscillator
+{
 public:
     float amplitude;
     float inc;
     float phase;
+    float freq;
+    float sampleRate;
+    float phaseBL;
+    
     
     void reset() {
         phase = 0.0f;
+        phaseBL = -0.5f;
     }
     
-    float nextSample() {
-        phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
+    float nextBandlimitedSample()
+    {
+        phaseBL += inc;
+        if (phaseBL >= 1.0f) {
+            phaseBL -= 1.0f;
         }
-        return amplitude * (2.0f * phase - 1.0f);
+        
+        float output = 0.0f;
+        float nyquist = sampleRate / 2.0f;
+        float h = freq;
+        float i = 1.0f;
+        float m = 0.6366197724f; // 2/pi
+        while (h < nyquist) {
+            output += m * std::sin(TWO_PI * phaseBL * i) / i;
+            h += freq;
+            i += 1.0f;
+            m = -m;
+        }
+        return output;
+    }
+    
+    float nextSample()
+    {
+        return amplitude * nextBandlimitedSample();
     }
 };
