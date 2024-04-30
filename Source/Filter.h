29f308f9b6f3a0a18184bb59a337c949a353eada
Johan Bremin <jbremin@hotmail.com>
2024-04-30 17:17:36 +0200

    Changed the neutral SVF to a more vibey moog style ladder filter from the JUCE dsp toolkit.

--- Source/Filter.h
+++ Source/Filter.h
@@ -10,6 +10,26 @@
 
 #pragma once
 
+#include <JuceHeader.h>
+
+class Filter : public juce::dsp::LadderFilter<float>
+{
+public:
+    void updateCoefficients(float cutoff, float Q)
+    {
+        setCutoffFrequencyHz(cutoff);
+        setResonance(std::clamp(Q / 30.0f, 0.0f, 1.0f));
+    }
+    
+    float render(float x)
+    {
+        updateSmoothers();
+        return processSample(x, 0);
+    }
+};
+
+/* // CYTOMIC SVF FILTER
+
 class Filter
 {
 public:
@@ -51,3 +71,4 @@ private:
     float g, k, a1, a2, a3; // filter coefficients
     float ic1eq, ic2eq;     // internal state
 };
+*/
